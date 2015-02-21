6eb02776abb23e96aaaf72b7333a3cb8389937d9
mattrothenberg <mattrothenberg@gmail.com>
2015-02-21 12:04:23 -0500

    Rename controllers and app to Arbr

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('arbr.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   // Form data for the login modal
