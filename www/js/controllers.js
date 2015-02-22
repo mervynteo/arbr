7eae0343d6ed81e9de5de09ea8f6c59b29908b40
mattrothenberg <mattrothenberg@gmail.com>
2015-02-21 20:23:33 -0500

    Added Find Me feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -35,104 +35,90 @@ angular.module('arbr.controllers', [])
 
 .controller('SplashCtrl', function($scope, $stateParams, $state) {
   $scope.fbLogin = function() {
-    $state.go('app.map');
+      openFB.login(
+          function(response) {
+              if (response.status === 'connected') {
+                  $state.go("app.map");
+              } else {
+                  alert('Facebook login failed');
+              }
+          },
+          {scope: 'email,publish_actions'});
   }
 })
 
 .controller('MapCtrl', function($scope, $ionicLoading, $compile) {
-      function initialize() {
-        var site = new google.maps.LatLng(55.9879314,-4.3042387);
-        var hospital = new google.maps.LatLng(55.8934378,-4.2201905);
-      
-        var mapOptions = {
-          streetViewControl:true,
-          center: site,
-          zoom: 18,
-          mapTypeId: google.maps.MapTypeId.TERRAIN
-        };
 
-        console.log("there");
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(37.7833,-122.4167);
+
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 12,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
         var map = new google.maps.Map(document.getElementById("map"),
             mapOptions);
-        
-        //Marker + infowindow + angularjs compiled ng-click
-        var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-        var compiled = $compile(contentString)($scope);
 
-        var infowindow = new google.maps.InfoWindow({
-          content: compiled[0]
-        });
+        // var marker = new google.maps.Marker({
+        //   position: myLatlng,
+        //   map: map,
+        //   title: 'Uluru (Ayers Rock)'
+        // });
 
-        var marker = new google.maps.Marker({
-          position: site,
-          map: map,
-          title: 'Strathblane (Job Location)'
-        });
-        
-        var hospitalRoute = new google.maps.Marker({
-          position: hospital,
-          map: map,
-          title: 'Hospital (Stobhill)'
-        });
-        
-        var infowindow = new google.maps.InfoWindow({
-             content:"Project Location"
-        });
-
-        infowindow.open(map,marker);
-        
-        var hospitalwindow = new google.maps.InfoWindow({
-             content:"Nearest Hospital"
-        });
-
-        hospitalwindow.open(map,hospitalRoute);
-       
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
+        // google.maps.event.addListener(marker, 'click', function() {
+        //   infowindow.open(map,marker);
+        // });
 
         $scope.map = map;
-        
-        var directionsService = new google.maps.DirectionsService();
-        var directionsDisplay = new google.maps.DirectionsRenderer();
+    };
 
-        var request = {
-            origin : site,
-            destination : hospital,
-            travelMode : google.maps.TravelMode.DRIVING
-        };
-        directionsService.route(request, function(response, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                directionsDisplay.setDirections(response);
-            }
-        });
+    ionic.Platform.ready(initialize);
 
-        directionsDisplay.setMap(map); 
-       
-      }
-      console.log('rawr');
-      google.maps.event.addDomListener(window, 'load', initialize());
-    
-      $scope.centerOnMe = function() {
+    // google.maps.event.addDomListener(window, 'load', initialize);
+
+    $scope.centerOnMe = function() {
         if(!$scope.map) {
-          return;
+            return;
         }
 
         $scope.loading = $ionicLoading.show({
           content: 'Getting current location...',
-          showBackdrop: false
+          showBackdrop: true
         });
+
         navigator.geolocation.getCurrentPosition(function(pos) {
-          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          var newPos = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+          $scope.map.setCenter(newPos);
           $scope.loading.hide();
+
+          var newMarker = new google.maps.Marker({
+            position: newPos,
+            icon: 'img/arbr-map-marker.png',
+            map: $scope.map,
+            title: 'rawrawr'
+          });
+
+          //Marker + infowindow + angularjs compiled ng-click
+          var contentString = "<div><a ng-click='clickTest()'>You are here!</a></div>";
+          var compiled = $compile(contentString)($scope);
+
+          var infowindow = new google.maps.InfoWindow({
+            content: compiled[0]
+          });
+
+          google.maps.event.addListener(newMarker, 'click', function() {
+            infowindow.open($scope.map,newMarker);
+          });
+
         }, function(error) {
           alert('Unable to get location: ' + error.message);
         });
-      };
-      
-      $scope.clickTest = function() {
-        alert('Example of infowindow with ng-click')
-      };
+    };
+
+    $scope.clickTest = function() {
+        alert('You clicked me!')
+    };
 })
 
 .controller('PlaylistsCtrl', function($scope) {
@@ -147,14 +133,5 @@ angular.module('arbr.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+
 });
-  //     openFB.login(
-  //         function(response) {
-  //             if (response.status === 'connected') {
-  //                 $state.go('app.map');
-  //             } else {
-  //                 alert('Facebook login failed');
-  //             }
-  //         },
-  //         {scope: 'email,publish_actions'});
-  // }
